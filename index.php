35f379145daa98a9cb31b0ac102bdb65458fdcec
seachelle18 <mhlchow@shaw.ca>
2022-11-19 13:02:30 -0700

    hello

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Habit Tracker</title>
+    <link rel="stylesheet" type="text/css" href="inc/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="habit tracker, mental health">
+    <meta name="description" content="Habit Tracker">
+    <meta name="author" content="Michelle Chow">
+</head>
+<body>
+    <?php include "header.html" ?>
+    <div class="login-box">
+        
+    </div>
+</body>
