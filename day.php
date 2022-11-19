620dcc7b4412d389be785ae306b59728345fd476
seachelle18 <mhlchow@shaw.ca>
2022-11-19 14:03:02 -0700

    questions added

--- day.php
+++ day.php
@@ -1,16 +1,16 @@
-<?php
-$quote = "This is a very inspirational quote. -Michelle Chow, 2022";
-$questionBank = ["How are you feeling today?", "How much energy do you have today?"];
-
+<?php 
+include "./header.html";
+include "./questionbank.php";
 ?>
-
-<?php include "./header.html"?>
 <body>
 <div class="block">
     <?php echo '"' . $quote . '"'; ?>
     <?php foreach ($questionBank as $question): ?>
-        <div class="question">
+        <div class="question-container">
             <?php echo $question; ?>
+        </div>
+        <div class="slide-container">
+            <input type="range" min="1" max="10" value="5" class="slider">
         </div>
     <?php endforeach; ?>
 </div>
