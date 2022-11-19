8d0308321102d1330d5fb6936c17ee6e9c543b88
seachelle18 <mhlchow@shaw.ca>
2022-11-19 14:50:35 -0700

    fun stuff

---
+++ questionbank.php
@@ -0,0 +1,5 @@
+<?php
+$quote = "This is a very inspirational quote. -Michelle Chow, 2022";
+$questionBank = ["How are you feeling right now?", "How much energy do you have today?", "What is this question?"];
+
+?>
