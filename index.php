82c640e9672d73e85645d03b5fcce76ce8623c98
shanellelck <shanellelck@outlook.com>
2022-11-19 14:32:31 -0700

    styled login page

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ session_start();
     <div class="login-box">
         <form class="login-form" method="POST">
             <label for="username" class="form-label">Username:</label>
-            <input type="text" id="username" name="username">
+            <input type="text" id="username" name="username" placeholder="Enter your username">
             <label for="password" class="form-label">Password:</label>
-            <input type="password" id="password" name="password">
+            <input type="password" id="password" name="password" placeholder="Enter your password">
             <input type="submit" value="Submit">
         </form>
 
