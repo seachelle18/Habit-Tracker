7a0df0103f385a1eafac89cd372ecd7f62a388f5
InfiniteTachyon <t60.david@gmail.com>
2022-11-20 02:15:16 -0700

    journal

--- journal.php
+++ journal.php
@@ -20,11 +20,11 @@ if (isset($_POST['submit'])) {
 $q1;
 $q2;
 $q3;
-echo $uname;
-echo $date;
-echo count($retrievedEntry);
+//echo $uname;
+//echo $date;
+//echo count($retrievedEntry);
 
-var_dump($retrievedEntry);
+//var_dump($retrievedEntry);
 
 // Values for questions 1-10
 if (count($retrievedEntry)>=1) {
@@ -33,7 +33,7 @@ if (count($retrievedEntry)>=1) {
     $q3 = $retrievedEntry[0]['question2'];
 }
 
-
+$test = 10;
 
 
 
@@ -43,17 +43,28 @@ if (count($retrievedEntry)>=1) {
 ?>
 
 <body>
-    <div class="block">
-        <form class="journal-retrieve"  method="POST">
-            <label for="journalDate">Select the date</label>
-            <input type="date" name="journalDate">
-            <input type="submit" name="submit" value="Submit">
-        </form>
-    </div>
+    <?php if (count($retrievedEntry) == 0): ?>
+
+        <div class="block">
+            <form class="journal-retrieve"  method="POST">
+                <label for="journalDate">Select the date</label>
+                <input type="date" name="journalDate">
+                <input type="submit" name="submit" value="Submit">
+            </form>
+        </div>
+    <?php endif ?>
+
     <?php if (count($retrievedEntry) == 1): ?>
         <div class="block">
             <!-- display some stuff -->
-            <input type="range" disabled min="0" max="10" value = $q1/>
+            <h1>Question 1: Placeholder</h1>
+            <input type="range"  min="0" max="10" value = <?=$q1?> disabled/>
+
+            <h1>Question 2: Placeholder</h1>
+            <input type="range"  min="0" max="10" value = <?=$q2?> disabled/>
+
+            <h1>Question 3: Placeholder</h1>
+            <input type="range"  min="0" max="10" value = <?=$q3?> disabled/>
         </div>
     <?php endif ?>
 
