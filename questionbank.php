22b65563acfd46fc63f42b5f51ee079a1d520501
InfiniteTachyon <t60.david@gmail.com>
2022-11-19 19:03:53 -0700

    added quoteAPI implementation

--- questionbank.php
+++ questionbank.php
@@ -1,5 +1,39 @@
 <?php
-$quote = "This is a very inspirational quote. -Michelle Chow, 2022";
+$quote = get_quote();
 $questionBank = ["How are you feeling right now?", "How much energy do you have today?", "What is this question?"];
+
+?>
+
+<?php
+
+function get_quote() {
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+      CURLOPT_URL => "https://zenquotes.io/api/quotes",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_POSTFIELDS => "",
+    ]);
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    $return = json_decode($response);
+
+    if ($err) {
+      echo "cURL Error #:" . $err;
+    } else {
+      echo ($return[0]->q);
+    }
+}
+
+
+
 
 ?>
