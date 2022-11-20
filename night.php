17e1c266530728161e493d8d60f8b97641d25ec5
seachelle18 <mhlchow@shaw.ca>
2022-11-19 18:59:28 -0700

    lots of fun stuff

--- night.php
+++ night.php
@@ -8,16 +8,16 @@ $goalResponses = ["placeholder1", "placeholder2", "placeholder3"];
 
 <body>
     <div class="block">
-        <form class="night-entry">
+    <form class="night-entry" method="POST">
         <?php foreach ($questionBank as $question): ?>
-            <div class="question-container">
+            <div class="question container">
                 <?php echo $question; ?>
             </div>
             <div class="slide-container">
-                <input type="range" min="1" max="10" value="5" class="slider">
+                <input type="range" min="1" max="10" value="5" class="slider" name=<?php echo 'question' . array_search($question, $questionBank)?>>
             </div>
         <?php endforeach; ?>
-        <p class="goal-question">Reflect on your mental health goals for today. Do you feel that you were successful in it?</p>
+        <p class="goal-question">Reflect on your mental health goals from today. Do you feel that you were successful in each one?</p>
         <?php foreach($goalResponses as $goal): ?>
             <div class="response-container">
                 <p class="goal-response-text"><?php echo $goal; ?></p>
