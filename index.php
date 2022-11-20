06b0b5b9bc1081c159c54c541ccecdaf2daa4be7
InfiniteTachyon <t60.david@gmail.com>
2022-11-19 20:43:08 -0700

    added prelim code for login.php

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 
-<?php include "header.html" ?>
+<?php include "header.php" ?>
+
+<?php
+    //session_start();
+
+
+?>
+
+
 <body>
     <div class="login-box">
-        <form class="login-form" method="POST">
+        <form class="login-form" method="POST" action="login.php">
             <label for="username" class="form-label">Username:</label>
             <input type="text" id="username" name="username" placeholder="Enter your username">
             <label for="password" class="form-label">Password:</label>
