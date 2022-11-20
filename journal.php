2947b2ee1170a60c6b5b7692eee8d24a56a78555
seachelle18 <mhlchow@shaw.ca>
2022-11-19 22:43:56 -0700

    hello cruel world

--- journal.php
+++ journal.php
@@ -1,3 +1,25 @@
 <?php include "./header.php";
 
+//Configure PDO
+$pdo = new PDO ('sqlite:C:\Users\michellec\xampp\htdocs\habit-tracker\Habit-Tracker\goal (2).db');
+$retrieve 
+
+if (isset($_POST['submit'])) {
+    $date = $_POST['journalDate'];
+    $sql = 'SELECT * FROM goals WHERE date = ?';
+    $statement = $pdo->prepare($sql);
+    $statement->execute($date);
+
+
+};
 ?>
+
+<body>
+    <div class="block">
+        <form class="journal-retrieve"  method="POST">
+            <label for="journalDate">Select the date</label>
+            <input type="date" name="journalDate">
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </div>
+</body>
